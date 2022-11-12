cf734fe0931c20227cd27a6cec55d01616194c06
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-11 20:33:43 -0500

    Input 1

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:codigo2_alerta/ui/general/colors.dart';
+import 'package:codigo2_alerta/ui/widgets/general_widget.dart';
 import 'package:codigo2_alerta/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 
@@ -12,14 +13,13 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset(
                   AssetData.logo,
                   height: 20.0,
                 ),
-                SizedBox(
-                  width: 6.0,
-                ),
+                spacingWidth6,
                 Text(
                   "Alerta App",
                   style: TextStyle(
@@ -30,6 +30,44 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            spacing6,
+            Text(
+              "Lorem ipsum dolor sit amet",
+              style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.normal,
+                  color: kFontPrimaryColor.withOpacity(0.75),
+              ),
+            ),
+            spacing6,
+            TextFormField(
+              style: TextStyle(
+                color: kFontPrimaryColor.withOpacity(0.80),
+                fontSize: 14.0,
+              ),
+              decoration: InputDecoration(
+                hintText: "Ingrese su DNI",
+                hintStyle: TextStyle(
+                  fontSize: 14.0,
+                  color: kFontPrimaryColor.withOpacity(0.5),
+                ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 15.0),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(
+                    color: kFontPrimaryColor.withOpacity(0.14),
+                    width: 0.9,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(
+                    color: kFontPrimaryColor.withOpacity(0.14),
+                    width: 0.9,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
