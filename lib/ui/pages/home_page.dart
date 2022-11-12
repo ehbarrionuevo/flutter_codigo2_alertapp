9e1ba7e22464d5b7bb2236402403e2e0bc679d9c
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-12 11:25:35 -0500

    HomePage 1.4

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:codigo2_alerta/ui/general/colors.dart';
 import 'package:codigo2_alerta/ui/widgets/general_widget.dart';
+import 'package:codigo2_alerta/ui/widgets/item_menu_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -39,52 +40,10 @@ class HomePage extends StatelessWidget {
                   crossAxisSpacing: 14.0,
                   mainAxisSpacing: 14.0,
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(14.0),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black.withOpacity(0.06),
-                              // color: Colors.red,
-                              offset: const Offset(0, 4),
-                              blurRadius: 12.0),
-                        ],
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(12.0),
-                            decoration: BoxDecoration(
-                              color: Color(0xfff72585).withOpacity(0.7),
-                              shape: BoxShape.circle,
-                            ),
-                            child: Icon(
-                              Icons.people,
-                              color: Colors.white,
-                            ),
-                          ),
-                          spacing6,
-                          Text(
-                            "Ciudadanos",
-                            style: TextStyle(
-                              color: kFontPrimaryColor.withOpacity(0.85),
-                              fontSize: 15.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      color: Colors.blue,
-                    ),
-                    Container(
-                      color: Colors.redAccent,
-                    ),
-                    Container(
-                      color: Colors.blue,
-                    ),
+                    ItemMenuWidget(),
+                    ItemMenuWidget(),
+                    ItemMenuWidget(),
+                    ItemMenuWidget(),
                   ],
                 ),
               ),
