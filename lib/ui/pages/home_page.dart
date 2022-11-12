ebbe71d00d523248622c01d84ebbc093dca64be8
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-12 10:39:05 -0500

    HomePage 1.0

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,5 +1,5 @@
-
-
+import 'package:codigo2_alerta/ui/general/colors.dart';
+import 'package:codigo2_alerta/ui/widgets/general_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,6 +7,34 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Bienvenido Ramón",
+                style: TextStyle(
+                  color: kFontPrimaryColor.withOpacity(0.80),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              spacing10,
+              Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+                style: TextStyle(
+                  color: kFontPrimaryColor.withOpacity(0.65),
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.normal,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
