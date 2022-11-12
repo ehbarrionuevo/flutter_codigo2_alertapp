087ccdc81f8e51669012574c1bfb631b2155cf0c
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-12 11:53:45 -0500

    Citizen Page 1

--- lib/ui/pages/citizen_page.dart
+++ lib/ui/pages/citizen_page.dart
@@ -1,4 +1,5 @@
-
+import 'package:codigo2_alerta/ui/general/colors.dart';
+import 'package:codigo2_alerta/ui/widgets/general_widget.dart';
 import 'package:flutter/material.dart';
 
 class CitizenPage extends StatelessWidget {
@@ -6,6 +7,66 @@ class CitizenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                "Listado de ciudadanos",
+                style: TextStyle(
+                  color: kFontPrimaryColor.withOpacity(0.80),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              spacing10,
+              Expanded(
+                child: ListView.separated(
+                  itemCount: 20,
+                  separatorBuilder: (context, index) => const Divider(
+                    indent: 12.0,
+                    endIndent: 12.0,
+                  ),
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        "Juan Manuel Lopez Montes",
+                        style: TextStyle(
+                          color: kFontPrimaryColor.withOpacity(0.80),
+                          fontSize: 15.0
+                        ),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          spacing3,
+                          Text(
+                            "Dirección: Av. Lima 12323",
+                            style: TextStyle(
+                                color: kFontPrimaryColor.withOpacity(0.55),
+                                fontSize: 13.0
+                            ),
+                          ),
+                          spacing3,
+                          Text(
+                            "Teléfono: 434111222",
+                            style: TextStyle(
+                                color: kFontPrimaryColor.withOpacity(0.55),
+                                fontSize: 13.0
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
