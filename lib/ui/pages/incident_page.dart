de096b55e46015e1e064ab8a24c2eb5f45e770fd
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-13 10:36:38 -0500

    Incident Page 3.4

--- lib/ui/pages/incident_page.dart
+++ lib/ui/pages/incident_page.dart
@@ -1,20 +1,65 @@
-
 import 'package:codigo2_alerta/models/incident_model.dart';
 import 'package:codigo2_alerta/services/api_service.dart';
 import 'package:codigo2_alerta/ui/general/colors.dart';
 import 'package:codigo2_alerta/ui/widgets/general_widget.dart';
 import 'package:flutter/material.dart';
 
+class IncidentPage extends StatefulWidget {
+  @override
+  State<IncidentPage> createState() => _IncidentPageState();
+}
 
-
-class IncidentPage extends StatelessWidget {
-
+class _IncidentPageState extends State<IncidentPage>
+    with TickerProviderStateMixin {
   ApiService apiService = ApiService();
+
+  showAddIncidentModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      transitionAnimationController: AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 650),
+      ),
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                "Registrar Incidente",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: kFontPrimaryColor.withOpacity(0.80),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              spacing10,
+              Text(
+                "Por favor selecciona un incidente para ser enviado a la central.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: kFontPrimaryColor.withOpacity(0.60),
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showAddIncidentModal(context);
+        },
+        backgroundColor: kBrandPrimaryColor,
+        child: Icon(Icons.add),
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -31,22 +76,21 @@ class IncidentPage extends StatelessWidget {
               spacing20,
               FutureBuilder(
                 future: apiService.getIncident(),
-                builder: (BuildContext context, AsyncSnapshot snap){
-                  if(snap.hasData){
+                builder: (BuildContext context, AsyncSnapshot snap) {
+                  if (snap.hasData) {
                     List<IncidentModel> listData = snap.data;
                     return Expanded(
                       child: ListView.separated(
                         physics: const BouncingScrollPhysics(),
                         itemCount: listData.length,
-                        separatorBuilder: (context, index)=> const Divider(),
-                        itemBuilder: (context, index){
+                        separatorBuilder: (context, index) => const Divider(),
+                        itemBuilder: (context, index) {
                           return ListTile(
                             title: Text(
                               listData[index].tipoIncidente.title,
                               style: TextStyle(
                                   color: kFontPrimaryColor.withOpacity(0.80),
-                                  fontSize: 15.0
-                              ),
+                                  fontSize: 15.0),
                             ),
                             subtitle: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
@@ -55,17 +99,17 @@ class IncidentPage extends StatelessWidget {
                                 Text(
                                   "Ciudadano: ${listData[index].datosCiudadano.nombres}",
                                   style: TextStyle(
-                                      color: kFontPrimaryColor.withOpacity(0.55),
-                                      fontSize: 13.0
-                                  ),
+                                      color:
+                                          kFontPrimaryColor.withOpacity(0.55),
+                                      fontSize: 13.0),
                                 ),
                                 spacing3,
                                 Text(
                                   "Teléfono: ${listData[index].datosCiudadano.telefono}",
                                   style: TextStyle(
-                                      color: kFontPrimaryColor.withOpacity(0.55),
-                                      fontSize: 13.0
-                                  ),
+                                      color:
+                                          kFontPrimaryColor.withOpacity(0.55),
+                                      fontSize: 13.0),
                                 ),
                               ],
                             ),
@@ -75,17 +119,17 @@ class IncidentPage extends StatelessWidget {
                                 Text(
                                   listData[index].hora,
                                   style: TextStyle(
-                                      color: kFontPrimaryColor.withOpacity(0.55),
-                                      fontSize: 13.0
-                                  ),
+                                      color:
+                                          kFontPrimaryColor.withOpacity(0.55),
+                                      fontSize: 13.0),
                                 ),
                                 spacing3,
                                 Text(
                                   listData[index].fecha,
                                   style: TextStyle(
-                                      color: kFontPrimaryColor.withOpacity(0.55),
-                                      fontSize: 13.0
-                                  ),
+                                      color:
+                                          kFontPrimaryColor.withOpacity(0.55),
+                                      fontSize: 13.0),
                                 ),
                               ],
                             ),
