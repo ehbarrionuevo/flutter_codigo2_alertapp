05c7d448c1dbb423e890ccfd403fd03be1d66d79
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-13 11:19:25 -0500

    Incident Page 3

--- lib/ui/pages/incident_page.dart
+++ lib/ui/pages/incident_page.dart
@@ -1,6 +1,7 @@
 import 'package:codigo2_alerta/models/incident_model.dart';
 import 'package:codigo2_alerta/services/api_service.dart';
 import 'package:codigo2_alerta/ui/general/colors.dart';
+import 'package:codigo2_alerta/ui/pages/modals/register_incident_modal.dart';
 import 'package:codigo2_alerta/ui/widgets/general_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -21,65 +22,7 @@ class _IncidentPageState extends State<IncidentPage>
         duration: const Duration(milliseconds: 650),
       ),
       builder: (BuildContext context) {
-        return Container(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Text(
-                "Registrar Incidente",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: kFontPrimaryColor.withOpacity(0.80),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              spacing10,
-              Text(
-                "Por favor selecciona un incidente para ser enviado a la central.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: kFontPrimaryColor.withOpacity(0.60),
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-              spacing20,
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(
-                    color: kFontPrimaryColor.withOpacity(0.12),
-                    width: 0.9,
-                  ),
-                ),
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton(
-                    borderRadius: BorderRadius.circular(16.0),
-                    elevation: 6,
-                    value: 1,
-                    isExpanded: true,
-                    items: [
-                      DropdownMenuItem(
-                        value: 1,
-                        child: Text(
-                          "Mordedura",
-                        ),
-                      ),
-                      DropdownMenuItem(
-                        value: 2,
-                        child: Text(
-                          "Robo",
-                        ),
-                      ),
-                    ],
-                    onChanged: (value) {},
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
+        return RegisterIncidentModal();
       },
     );
   }
