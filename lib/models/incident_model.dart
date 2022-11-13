742da6e40e3e53ef41c35df7a299a26415a1f266
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-13 09:58:07 -0500

    Incident Page 3.1

--- lib/models/incident_model.dart
+++ lib/models/incident_model.dart
@@ -1,96 +1,42 @@
+import 'package:codigo2_alerta/models/incident_type_model.dart';
+
 class IncidentModel {
   IncidentModel({
-    this.id,
-    this.tipoIncidente,
-    this.longitud,
-    this.latitud,
-    this.fecha,
-    this.hora,
-    this.ciudadanoNombre,
-    this.fechaCreacion,
-    this.datosCiudadano,
-    this.estadoDisplay,
-    this.tipoOrigenDisplay,
-    this.personalDisplay,
-    this.unidadDisplay,
-    this.usuarioDisplay,
-    this.estado,
-    this.tipoOrigen,
-    this.atendido,
-    this.atendidoSerenazgo,
-    this.calificacion,
-    this.calificacionCiudadano,
-    this.observacion,
-    this.nombres,
-    this.dni,
-    this.telefono,
-    this.imagen,
-    this.ciudadano,
-    this.usuario,
-    this.unidad,
-    this.personalSeguridad,
+    required this.id,
+    required this.tipoIncidente,
+    required this.longitud,
+    required this.latitud,
+    required this.fecha,
+    required this.hora,
+    required this.fechaCreacion,
+    required this.datosCiudadano,
+    required this.estado,
+    required this.tipoOrigen,
   });
 
   int id;
-  TipoIncidente tipoIncidente;
+  IncidentTypeModel tipoIncidente;
   double longitud;
   double latitud;
   String fecha;
   String hora;
-  String ciudadanoNombre;
   String fechaCreacion;
   DatosCiudadano datosCiudadano;
-  String estadoDisplay;
-  String tipoOrigenDisplay;
-  dynamic personalDisplay;
-  dynamic unidadDisplay;
-  dynamic usuarioDisplay;
   String estado;
   String tipoOrigen;
-  bool atendido;
-  bool atendidoSerenazgo;
-  int calificacion;
-  int calificacionCiudadano;
-  dynamic observacion;
-  dynamic nombres;
-  dynamic dni;
-  dynamic telefono;
-  dynamic imagen;
-  int ciudadano;
-  dynamic usuario;
-  dynamic unidad;
-  dynamic personalSeguridad;
+
 
   factory IncidentModel.fromJson(Map<String, dynamic> json) => IncidentModel(
     id: json["id"],
-    tipoIncidente: TipoIncidente.fromJson(json["tipoIncidente"]),
+    tipoIncidente: IncidentTypeModel.fromJson(json["tipoIncidente"]),
     longitud: json["longitud"].toDouble(),
     latitud: json["latitud"].toDouble(),
     fecha: json["fecha"],
     hora: json["hora"],
-    ciudadanoNombre: json["ciudadanoNombre"],
     fechaCreacion: json["fechaCreacion"],
     datosCiudadano: DatosCiudadano.fromJson(json["datosCiudadano"]),
-    estadoDisplay: json["estado_display"],
-    tipoOrigenDisplay: json["tipoOrigen_display"],
-    personalDisplay: json["personal_display"],
-    unidadDisplay: json["unidad_display"],
-    usuarioDisplay: json["usuario_display"],
     estado: json["estado"],
     tipoOrigen: json["tipoOrigen"],
-    atendido: json["atendido"],
-    atendidoSerenazgo: json["atendidoSerenazgo"],
-    calificacion: json["calificacion"],
-    calificacionCiudadano: json["calificacionCiudadano"],
-    observacion: json["observacion"],
-    nombres: json["nombres"],
-    dni: json["dni"],
-    telefono: json["telefono"],
-    imagen: json["imagen"],
-    ciudadano: json["ciudadano"],
-    usuario: json["usuario"],
-    unidad: json["unidad"],
-    personalSeguridad: json["personalSeguridad"],
   );
 
   Map<String, dynamic> toJson() => {
@@ -100,29 +46,10 @@ class IncidentModel {
     "latitud": latitud,
     "fecha": fecha,
     "hora": hora,
-    "ciudadanoNombre": ciudadanoNombre,
     "fechaCreacion": fechaCreacion,
     "datosCiudadano": datosCiudadano.toJson(),
-    "estado_display": estadoDisplay,
-    "tipoOrigen_display": tipoOrigenDisplay,
-    "personal_display": personalDisplay,
-    "unidad_display": unidadDisplay,
-    "usuario_display": usuarioDisplay,
     "estado": estado,
     "tipoOrigen": tipoOrigen,
-    "atendido": atendido,
-    "atendidoSerenazgo": atendidoSerenazgo,
-    "calificacion": calificacion,
-    "calificacionCiudadano": calificacionCiudadano,
-    "observacion": observacion,
-    "nombres": nombres,
-    "dni": dni,
-    "telefono": telefono,
-    "imagen": imagen,
-    "ciudadano": ciudadano,
-    "usuario": usuario,
-    "unidad": unidad,
-    "personalSeguridad": personalSeguridad,
   };
 }
 
@@ -130,9 +57,9 @@ class IncidentModel {
 
 class DatosCiudadano {
   DatosCiudadano({
-    this.nombres,
-    this.dni,
-    this.telefono,
+    required this.nombres,
+    required this.dni,
+    required this.telefono,
   });
 
   String nombres;
@@ -149,43 +76,5 @@ class DatosCiudadano {
     "nombres": nombres,
     "dni": dni,
     "telefono": telefono,
-  };
-}
-
-
-
-class TipoIncidente {
-  TipoIncidente({
-    this.id,
-    this.value,
-    this.text,
-    this.titulo,
-    this.area,
-    this.nivel,
-  });
-
-  int id;
-  int value;
-  String text;
-  String titulo;
-  String area;
-  String nivel;
-
-  factory TipoIncidente.fromJson(Map<String, dynamic> json) => TipoIncidente(
-    id: json["id"],
-    value: json["value"],
-    text: json["text"],
-    titulo: json["titulo"],
-    area: json["area"],
-    nivel: json["nivel"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "id": id,
-    "value": value,
-    "text": text,
-    "titulo": titulo,
-    "area": area,
-    "nivel": nivel,
   };
 }
