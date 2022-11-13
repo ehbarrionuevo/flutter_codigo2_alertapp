d33ade76a40cf05f03b59ab0d14fa6064d3a9986
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-13 10:20:36 -0500

    Incident Page 3.3

--- lib/ui/pages/incident_page.dart
+++ lib/ui/pages/incident_page.dart
@@ -1,6 +1,8 @@
 
+import 'package:codigo2_alerta/models/incident_model.dart';
 import 'package:codigo2_alerta/services/api_service.dart';
 import 'package:codigo2_alerta/ui/general/colors.dart';
+import 'package:codigo2_alerta/ui/widgets/general_widget.dart';
 import 'package:flutter/material.dart';
 
 
@@ -26,16 +28,68 @@ class IncidentPage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              spacing20,
               FutureBuilder(
                 future: apiService.getIncident(),
                 builder: (BuildContext context, AsyncSnapshot snap){
                   if(snap.hasData){
+                    List<IncidentModel> listData = snap.data;
                     return Expanded(
                       child: ListView.separated(
-                        itemCount: 10,
+                        physics: const BouncingScrollPhysics(),
+                        itemCount: listData.length,
                         separatorBuilder: (context, index)=> const Divider(),
                         itemBuilder: (context, index){
-                          return Text("Hola");
+                          return ListTile(
+                            title: Text(
+                              listData[index].tipoIncidente.title,
+                              style: TextStyle(
+                                  color: kFontPrimaryColor.withOpacity(0.80),
+                                  fontSize: 15.0
+                              ),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                spacing3,
+                                Text(
+                                  "Ciudadano: ${listData[index].datosCiudadano.nombres}",
+                                  style: TextStyle(
+                                      color: kFontPrimaryColor.withOpacity(0.55),
+                                      fontSize: 13.0
+                                  ),
+                                ),
+                                spacing3,
+                                Text(
+                                  "Teléfono: ${listData[index].datosCiudadano.telefono}",
+                                  style: TextStyle(
+                                      color: kFontPrimaryColor.withOpacity(0.55),
+                                      fontSize: 13.0
+                                  ),
+                                ),
+                              ],
+                            ),
+                            trailing: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  listData[index].hora,
+                                  style: TextStyle(
+                                      color: kFontPrimaryColor.withOpacity(0.55),
+                                      fontSize: 13.0
+                                  ),
+                                ),
+                                spacing3,
+                                Text(
+                                  listData[index].fecha,
+                                  style: TextStyle(
+                                      color: kFontPrimaryColor.withOpacity(0.55),
+                                      fontSize: 13.0
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
                         },
                       ),
                     );
