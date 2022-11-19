6959bb088174bccd47d92c629e6696a9694ef521
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-18 19:46:48 -0500

    News 1.1

--- lib/ui/pages/news_register_page.dart
+++ lib/ui/pages/news_register_page.dart
@@ -1,8 +1,14 @@
 import 'package:codigo2_alerta/ui/general/colors.dart';
+import 'package:codigo2_alerta/ui/widgets/button_custom_widget.dart';
+import 'package:codigo2_alerta/ui/widgets/general_widget.dart';
+import 'package:codigo2_alerta/ui/widgets/textfield_custom_widget.dart';
 import 'package:flutter/material.dart';
 
 class NewsRegisterPage extends StatelessWidget {
-  const NewsRegisterPage({Key? key}) : super(key: key);
+
+
+  final TextEditingController _titleController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +19,39 @@ class NewsRegisterPage extends StatelessWidget {
         iconTheme: IconThemeData(
           color: kFontPrimaryColor,
         ),
+        leading: IconButton(
+          onPressed: (){
+            //...
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.share),
+        ),
         title: Text(
           "Registrar Noticia",
           style: TextStyle(
             color: kFontPrimaryColor,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFieldCustomWidget(
+                controller: _titleController,
+                label: "Título",
+                hintText: "Ingresa un título",
+              ),
+              spacing14,
+              TextFieldCustomWidget(
+                controller: _titleController,
+                label: "Link",
+                hintText: "Ingresa un link",
+              ),
+              spacing14,
+              ButtonCustomWidget(text: "Registrar noticia", onTap: (){},),
+            ],
           ),
         ),
       ),
